6e47f42f8c364b715d780fea105d7cf948c33d04
alessandrohenriqueramos <alessandrohenriqueramos@users.noreply.github.com>
2021-04-07 23:55:59 +0000

    Deploying to gh-pages from @ techmmunity/techmmunity.github.io@689a84b477a97ae4cbf5bc78e4b3a2b72a2ff0aa 🚀

---
+++ _next/static/n1aSjgREgifdHvcsxoBh5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-fdaebe1bcab1aeeabd4e.js"],"/_error":["static/chunks/pages/_error-3ec221f22e625cfb314d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
