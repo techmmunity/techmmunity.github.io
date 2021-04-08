fb8d79a45421dcc0ac4d568c0ea73c49cd5ebc46
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-08 08:00:33 -0300

    update head

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -6,7 +6,7 @@ export const Head = () => (
 		<GoogleFonts href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" />
 		<HeadNext>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			<title>Template</title>
+			<title>🧑‍💻 | Techmmunity</title>
 		</HeadNext>
 	</>
 );
