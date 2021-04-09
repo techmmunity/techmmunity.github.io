c49794c1feaad076a4c434d1e11a294bcb2ff33d
alessandrohenriqueramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-09 10:23:35 +0000

    Deploying to gh-pages from @ techmmunity/techmmunity.github.io@624b4cd82b323236c861c378f99418bc61135840 🚀

---
+++ _next/static/O9I3P2W-Ud1hk8l87WXlG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ecd53b08ed3c0ea2aa1f.js"],"/_error":["static/chunks/pages/_error-3ec221f22e625cfb314d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/cy8BH82dHZJ0fFPlQKmq7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a38589bc0c3d8ad5f588.js"],"/_error":["static/chunks/pages/_error-3ec221f22e625cfb314d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
