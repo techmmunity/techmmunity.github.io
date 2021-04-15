24360ebc6aeac21af6743a7a60b15c309c9590e6
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-15 11:53:17 -0300

    Remove more about section

--- src/components/MoreAboutSection/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Container, Image } from "./styles";
-
-export const MoreAboutSection: React.FC = () => (
-	<Container id="about-us">
-		<Image src="/pexels-fauxels.webp" />
-	</Container>
-);
