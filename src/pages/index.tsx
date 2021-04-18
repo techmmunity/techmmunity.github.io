40231112641e19092057b9f9287cf81004d53ff4
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-17 21:38:51 -0300

    Remove technologies section (#31)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import { FeedbackSection } from "components/FeedbackSection";
 import { Line } from "components/Line";
 import { MadeOfSection } from "components/MadeOfSection";
 import { MainSection } from "components/MainSection";
-import { TechnologiesSection } from "components/TechnologiesSection";
 
 import { Container } from "styles/pages/Home";
 
@@ -11,8 +10,6 @@ const Home: React.FC = () => (
 		<MainSection />
 		<Line />
 		<MadeOfSection />
-		<Line />
-		<TechnologiesSection />
 		<Line />
 		<FeedbackSection />
 		<Line />
