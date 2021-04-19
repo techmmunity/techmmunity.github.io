5b1489371ec9498b3a6e13df9ae97f90734cfd51
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-19 16:58:21 -0300

    Add robotic engineering text to made for everyone section (#38)

--- src/components/MadeForEveryoneSection/components/Cards/index.tsx
+++ src/components/MadeForEveryoneSection/components/Cards/index.tsx
@@ -46,8 +46,8 @@ export const Cards: React.FC<Props> = ({ currentCard }) => (
 		<Card
 			isCurrent={currentCard === CardEnum.ROBOTIC}
 			invertOrder={CardEnum.ROBOTIC % 2 === 0}
-			title="Optimize and scale, easy to start"
-			description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+			title="Our robotic engineering division"
+			description="Robotic engineering is a constantly expanding sector, both for industries and the entertainment of the general population. With our technological advancement, the demand for professionals capable of working in this area should grow more and more, so we created our technology sector to prepare professionals for this future demand."
 			url="#"
 			image="pexels-robotic.webp"
 			alt="robotic"
