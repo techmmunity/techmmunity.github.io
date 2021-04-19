7c8f16ec8a8b61ef3be41ee14e8ebfde2e87091d
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-19 16:40:45 -0300

    Add programming text to made for everyone section (#41)

--- src/components/MadeForEveryoneSection/components/Cards/index.tsx
+++ src/components/MadeForEveryoneSection/components/Cards/index.tsx
@@ -19,8 +19,8 @@ export const Cards: React.FC<Props> = ({ currentCard }) => (
 		<Card
 			isCurrent={currentCard === CardEnum.DEV}
 			invertOrder={CardEnum.DEV % 2 === 0}
-			title="Optimize and scale, easy to start"
-			description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+			title="Our programming division"
+			description="With an increasingly demanding market, professionals who are thinking of working in the area need to know more and more technologies from different areas. With that in mind, we created our programming division, which helps both the beginners and the more experienced to better understand the most used technologies on the market."
 			url="#"
 			image="pexels-programming.webp"
 			alt="programming"
