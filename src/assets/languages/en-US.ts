61849d12ec0685142c1c1c23ef54001cdfd2e725
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-19 22:22:16 -0300

    Add first feedback

---
+++ src/assets/languages/en-US.ts
@@ -0,0 +1,24 @@
+export const EN = {
+	feedbacks: [
+		{
+			id: 1,
+			feedback: `The community is very welcoming, I was always afraid to ask questions in other communities for fear of being "too stupid questions", but at Techmmunity, people understand that nobody is born knowing, answer even the stupid questions without making fun of whoever is asking. It's cool to be able to ask questions without fear.`,
+			url: "https://github.com/alessandrohenriqueramos/",
+			author: "Alessandro Ramos",
+		},
+		{
+			id: 2,
+			feedback:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, qui. Omnis illum assumenda voluptates corrupti fuga iste? Totam repellat exercitationem sint sit iusto laboriosam, asperiores ad temporibus, at voluptates suscipit.",
+			url: "#",
+			author: "People 2",
+		},
+		{
+			id: 3,
+			feedback:
+				"Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, qui. Omnis illum assumenda voluptates corrupti fuga iste? Totam repellat exercitationem sint sit iusto laboriosam, asperiores ad temporibus, at voluptates suscipit.",
+			url: "#",
+			author: "People 3",
+		},
+	],
+};
