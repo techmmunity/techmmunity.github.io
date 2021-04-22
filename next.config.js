49ac84149a5793b61f8fe05e03af97fc0bcf01a1
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-22 09:10:50 -0300

    Remove @svgr/webpack

--- next.config.js
+++ next.config.js
@@ -3,12 +3,4 @@ module.exports = {
 		webpack5: true,
 	},
 	assetPrefix: "/",
-	webpack: config => {
-		config.module.rules.push({
-			test: /\.svg$/,
-			use: ["@svgr/webpack"],
-		});
-
-		return config;
-	},
 };
