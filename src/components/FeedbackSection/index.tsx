28ca926af62cdf64d6dd9a8f56f75446e20cca1a
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-04-23 20:58:23 -0300

    Convert cards into array

--- src/components/FeedbackSection/index.tsx
+++ src/components/FeedbackSection/index.tsx
@@ -4,6 +4,27 @@ import { EN } from "assets/languages/en-US";
 
 import { Container, Description, Title, Subtitle, Cards } from "./styles";
 
+const feedbacksCards = [
+	{
+		id: 1,
+		feedback: EN.feedbackSection.feedbacks.alessandroRamos.feedback,
+		url: "https://github.com/alessandrohenriqueramos/",
+		author: "Alessandro Ramos",
+	},
+	{
+		id: 2,
+		feedback: EN.feedbackSection.feedbacks.diegoPinheiro.feedback,
+		url: "https://github.com/Ch1ron",
+		author: "Diego Pinheiro",
+	},
+	{
+		id: 3,
+		feedback: EN.feedbackSection.feedbacks.plactronic.feedback,
+		url: "https://www.youtube.com/channel/UCbPFdBr51_a_P_vM8z6Vu4w",
+		author: "Plactronic",
+	},
+];
+
 export const FeedbackSection: React.FC = () => (
 	<Container>
 		<Description>
@@ -11,8 +32,8 @@ export const FeedbackSection: React.FC = () => (
 			<Subtitle>See what our members have to talk about us.</Subtitle>
 		</Description>
 		<Cards>
-			{EN.feedbacks.map(({ id, feedback, url, author }) => (
-				<Card key={id} feedback={feedback} url={url} author={author} />
+			{feedbacksCards.map(({ id, ...props }) => (
+				<Card key={id} {...props} />
 			))}
 		</Cards>
 	</Container>
--- src/components/MadeOfSection/index.tsx
+++ src/components/MadeOfSection/index.tsx
@@ -4,6 +4,8 @@ import { TiGroup } from "react-icons/ti";
 
 import { Card } from "./components/Card";
 
+import { EN } from "assets/languages/en-US";
+
 import {
 	Container,
 	Description,
@@ -12,6 +14,27 @@ import {
 	Strong,
 	Cards,
 } from "./styles";
+
+const madeOfCards = [
+	{
+		id: 1,
+		Icon: FiCpu,
+		title: EN.madeOfSection.cards.technology.title,
+		description: EN.madeOfSection.cards.technology.description,
+	},
+	{
+		id: 2,
+		Icon: TiGroup,
+		title: EN.madeOfSection.cards.community.title,
+		description: EN.madeOfSection.cards.community.description,
+	},
+	{
+		id: 3,
+		Icon: AiFillLike,
+		title: EN.madeOfSection.cards.diversity.title,
+		description: EN.madeOfSection.cards.diversity.description,
+	},
+];
 
 export const MadeOfSection: React.FC = () => (
 	<Container>
@@ -22,21 +45,9 @@ export const MadeOfSection: React.FC = () => (
 			</Subtitle>
 		</Description>
 		<Cards>
-			<Card
-				Icon={FiCpu}
-				title="Technology"
-				description="We talk about every technology sector, like programming, graphic design, sound design, robotics, entrepreneurship, human resources, and and many other topics."
-			/>
-			<Card
-				Icon={TiGroup}
-				title="Community"
-				description="We are friendly and without prejudice, we help everyone, regardless of the question! After all, we've all been beginners once, right?"
-			/>
-			<Card
-				Icon={AiFillLike}
-				title="Diversity"
-				description="Our community is made up of all types of people, who provide a different point of view on a problem, always leading us to different discussions and new learnings."
-			/>
+			{madeOfCards.map(({ id, ...props }) => (
+				<Card key={id} {...props} />
+			))}
 		</Cards>
 	</Container>
 );
