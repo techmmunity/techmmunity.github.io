9df31975e474afa20da1a18752254ddc93df15fe
Henrique Leite <henriqueleite616@gmail.com>
2021-05-06 20:05:19 +0100

    Removed preconnect link from _document tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -74,7 +74,6 @@ export default class MyDocument extends Document {
 					<meta name="og:url" content="https://techmmunity.github.io/" />
 					<meta name="og:locale" content={htmlLang} />
 					<link rel="cannonical" href="https://techmmunity.github.io/" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
 						rel="stylesheet"
