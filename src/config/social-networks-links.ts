c1dfc4597f4863433f547cf832c6b22526f04762
Thiago Spart <user000.000resu@gmail.com>
2021-05-07 01:32:48 -0300

    Change linkedin url

--- src/config/social-networks-links.ts
+++ src/config/social-networks-links.ts
@@ -4,6 +4,6 @@ export const SocialNetworksLinks = {
 	YOUTUBE: "https://www.youtube.com/channel/UCl322bbCTdwW-eOEO4J0Zxw",
 	FACEBOOK: "https://www.facebook.com/techmmunity/",
 	TWITTER: "https://twitter.com/techmmunity1",
-	LINKEDIN: "https://www.linkedin.com/company/71251333/",
+	LINKEDIN: "https://www.linkedin.com/company/techmmunity/",
 	INSTAGRAM: "https://www.instagram.com/techmmunity/",
 };
