f4cb15b8ca11a7acbf25f1d46aed985abbd2e5d6
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-05-17 08:34:29 -0300

    Change emoji from title (#126)

--- src/web/pages/App/components/Head/index.tsx
+++ src/web/pages/App/components/Head/index.tsx
@@ -1,15 +1,8 @@
-import { useTranslation } from "next-i18next";
 import HeadNext from "next/head";
 
-export const Head = () => {
-	const { t } = useTranslation();
-
-	return (
-		<>
-			<HeadNext>
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<title>{t("head.title")}</title>
-			</HeadNext>
-		</>
-	);
-};
+export const Head = () => (
+	<HeadNext>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>&#x1F9D1;&#x1F4BB; | Techmmunity</title>
+	</HeadNext>
+);
