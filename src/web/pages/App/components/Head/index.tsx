199d9eced1c7380961977f561733d8455f7d243b
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-05-28 08:22:22 -0300

    Fix title

--- src/web/pages/App/components/Head/index.tsx
+++ src/web/pages/App/components/Head/index.tsx
@@ -3,6 +3,6 @@ import HeadNext from "next/head";
 export const Head = () => (
 	<HeadNext>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>&#x1F9D1;&#x1F4BB; | Techmmunity</title>
+		<title>Techmmunity</title>
 	</HeadNext>
 );
