51b553940f6c01c9f01720cfe754bcb81d14727f
Thiago Spart <user000.000resu@gmail.com>
2021-06-06 15:17:20 -0300

    TechmmunityDivisionSection list

--- src/web/pages/Home/components/TechmmunityDivisionsSection/components/Card/styles.ts
+++ src/web/pages/Home/components/TechmmunityDivisionsSection/components/Card/styles.ts
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 import { COLORS } from "assets/colors";
 
-export const Container = styled.div<{
+export const Container = styled.li<{
 	isCurrent: boolean;
 	invertOrder: boolean;
 }>`
